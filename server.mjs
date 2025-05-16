a98249839dd64ba987aa4777eee8a814600ad925
Jiri Benes <benes.jiri@shoppingarena.eu>
2025-05-16 03:18:49 +0300

    update for build

--- server.mjs
+++ server.mjs
@@ -1,18 +1,15 @@
 import { createServer } from 'http'
-import { parse } from 'url'
 import next from 'next'
 
-const port = parseInt(process.env.PORT || '3000', 10)
+const port = process.env.PORT || 3000
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev: false })
+const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
     createServer((req, res) => {
-        const parsedUrl = parse(req.url, true)
-        handle(req, res, parsedUrl)
-    }).listen(port, (err) => {
-        if (err) throw err
+        handle(req, res)
+    }).listen(port, () => {
         console.log(`> Ready on http://localhost:${port}`)
     })
 })
