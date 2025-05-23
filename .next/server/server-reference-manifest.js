168112f7de8c221e77589afb28b6dbc6b3ba0f3c
Jiri Benes <benes.jiri@shoppingarena.eu>
2025-05-23 15:26:11 +0300

    update after build

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"/tCjdFHko0/e280qWELgXmsCdA1vefc1HR+eSBGWgNU=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
